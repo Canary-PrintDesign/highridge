a3f0d503b876ad1a0196e044c2ac28f151e8a2ae
Sinetheta <k.attfield@gmail.com>
2017-05-28 14:19:19 -0700

    Use webpack for js and css

---
+++ README.md
@@ -0,0 +1,18 @@
+# Highridge
+A website for Highridge Homes
+
+## Prerequisites
+
+- Ruby ~2.4.1
+- Node
+- [Yarn][yarn installation]
+
+## Project Setup
+
+```sh
+yarn install
+bundle install
+bundle exec middleman server
+```
+
+[yarn installation]: https://yarnpkg.com/en/docs/install
--- config.rb
+++ config.rb
@@ -5,42 +5,16 @@ activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
 end
 
-# Layouts
-# https://middlemanapp.com/basics/layouts/
-
-# Per-page layout changes
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
-# With alternative layout
-# page '/path/to/file.html', layout: 'other_layout'
+activate :external_pipeline,
+  name: :webpack,
+  command: build? ? 'yarn run build': 'yarn start',
+  source: '.tmp/dist',
+  latency: 1
 
-# Proxy pages
-# https://middlemanapp.com/advanced/dynamic-pages/
-
-# proxy(
-#   '/this-page-has-no-template.html',
-#   '/template-file.html',
-#   locals: {
-#     which_fake_page: 'Rendering a fake page with a local variable'
-#   },
-# )
-
-# Helpers
-# Methods defined in the helpers block are available in templates
-# https://middlemanapp.com/basics/helper-methods/
-
-# helpers do
-#   def some_helper
-#     'Helping'
-#   end
-# end
-
-# Build-specific configuration
-# https://middlemanapp.com/advanced/configuration/#environment-specific-settings
-
-# configure :build do
-#   activate :minify_css
-#   activate :minify_javascript
-# end
+set :css_dir, 'assets/stylesheets'
+set :js_dir, 'assets/javascripts'
+set :images_dir, 'images'
