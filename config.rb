59262e89eeb640555c3ee716c88f60b7444a3ae3
adamish717 <adam@piechnik.ca>
2017-10-16 18:13:02 -0700

    Add S3 Sync

--- config.rb
+++ config.rb
@@ -27,3 +27,18 @@ set :js_dir, 'assets/javascripts'
 set :images_dir, 'images'
 
 require 'susy'
+
+activate :s3_sync do |s3_sync|
+  s3_sync.bucket                     = 'highridge.canaryprint.ca'
+  s3_sync.region                     = 'us-west-2'
+  s3_sync.aws_access_key_id          = 'REDACTED: Use .s3_sync file with info in it'
+  s3_sync.aws_secret_access_key      = 'REDACTED: Use .s3_sync file with info in it'
+  s3_sync.delete                     = true
+  s3_sync.prefer_gzip                = false
+  s3_sync.acl                        = 'public-read'
+  s3_sync.encryption                 = false
+  s3_sync.prefix                     = ''
+  s3_sync.version_bucket             = false
+end
+
+require 'mime-types'
