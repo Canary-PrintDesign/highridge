4d7ac058da4d16caa77de7fa3666e05e64b56292
Kirsten Dodd <kirsten.jvd@gmail.com>
2017-12-01 03:06:44 -0800

    Update to lines and contact form

--- assets/javascripts/site.js
+++ assets/javascripts/site.js
@@ -1,39 +1,36 @@
-require('jquery');
 
-console.log('Welcome to Highridge Homes!');
+// Set up an event listener for the contact form.
+$('form').submit(function(e) {
 
-  // Set up an event listener for the contact form.
-  $('form').submit(function(e) {
+  e.preventDefault();
 
-    e.preventDefault();
+  // Serialize the form data.
+  var formData = $('form').serialize();
+  var formMessages = $('#form-messages');
 
-    // Serialize the form data.
-    var formData = $('form').serialize();
-    var formMessages = $('#form-messages');
+  $.ajax({
+    type: "POST",
+    url: $('form').attr('action'),
+    data: formData,
+    success: function(){
+      $('form').removeClass('error');
+      $('form').addClass('success');
+      $(formMessages).addClass('success');
+      $(formMessages).removeClass('error');
+      $('form button').text('Successfully Sent!');
+      $('form').find('textarea, :text, :email').val('');
+      console.log('success');
+    },
+    error: function(){
 
-    $.ajax({
-      type: "POST",
-      url: $('form').attr('action'),
-      data: formData,
-      success: function(){
-        $('form').removeClass('error');
-        $('form').addClass('success');
-        $(formMessages).addClass('success');
-        $(formMessages).removeClass('error');
-        $('form button').text('Successfully Sent!');
-        $('form').find('textarea, :text, :email').val('');
-        console.log('success');
-      },
-      error: function(){
+      $('form').removeClass('success');
+      $('form').addClass('error');
+      $(formMessages).addClass('error');
+      $(formMessages).removeClass('success');
+      $('form button').text('Please Try Again');
+      $(formMessages).text('Oops! An error occured and your message could not be sent.');
 
-        $('form').removeClass('success');
-        $('form').addClass('error');
-        $(formMessages).addClass('error');
-        $(formMessages).removeClass('success');
-        $('form button').text('Please Try Again');
-        $(formMessages).text('Oops! An error occured and your message could not be sent.');
+    }
+  })
 
-      }
-    })
-
-  });
+});
